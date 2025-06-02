26952ad49a7fa18a8c9184e99baadd520bbf6a07
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-06-02 18:53:27 +0100

    First failing test

---
+++ src/test/java/com/kenny/wenny/tdd/StringReverserTest.java
@@ -0,0 +1,16 @@
+package com.kenny.wenny.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringReverserTest {
+    @Test
+    void when_passingInString_then_returnsStringReversed() {
+        var reverser = new StringReverser();
+
+        var result = reverser.reverse("abc");
+
+        assertEquals("cba", result);
+    }
+}
