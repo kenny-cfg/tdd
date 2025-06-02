a012dc7f6c8d0affd918182f596fb48969c4b749
Kenny Hung (he) <kenny.cfg@outlook.com>
2025-06-02 19:11:30 +0100

    MathUtil tests

---
+++ src/test/java/com/kenny/wenny/tdd/MathUtilTest.java
@@ -0,0 +1,25 @@
+package com.kenny.wenny.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilTest {
+    @Test
+    void when_providedTwoPositiveNumbers_then_returnsSum() {
+        var expectedSum = 42;
+
+        var actual = MathUtil.add(22, 20);
+
+        assertEquals(expectedSum, actual);
+    }
+
+    @Test
+    void when_providedNegativeAndZeroNumbers_then_returnsSum() {
+        var expectedSum = -42;
+
+        var actual = MathUtil.add(-42, 0);
+
+        assertEquals(expectedSum, actual);
+    }
+}
